c2287c1d940e23d3444ab7e6adad7a3e00a4d06d
Shahar Femron <59650633+Shahar-Satori@users.noreply.github.com>
2021-01-11 11:06:39 +0200

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -15,9 +15,9 @@ curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/e
 # open the tar.gz
 tar zxfv eksctl.tar.gz
 # create the cluster yaml
-sed "s/AWS_REGION/${AWS_REGION}/g;s/ARN_NUMBER/${ARN_NUMBER}/g;s/CLUSTER_NAME/${CLUSTER_NAME}/g;s/PRIVATE_SUB1_ID/${PRIVATE_SUB1_ID}/g;s/PRIVATE_SUB2_ID/${PRIVATE_SUB2_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB1_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB2_ID}/g;s/VPC_ID/${VPC_ID}/g" cluster-template.yaml > ${AWS_REGION}-${CLUSTER_NAME}.yaml
+sed "s/AWS_REGION/${AWS_REGION}/g;s/ARN_NUMBER/${ARN_NUMBER}/g;s/CLUSTER_NAME/${CLUSTER_NAME}/g;s/PRIVATE_SUB1_ID/${PRIVATE_SUB1_ID}/g;s/PRIVATE_SUB2_ID/${PRIVATE_SUB2_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB1_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB2_ID}/g;s/VPC_ID/${VPC_ID}/g;s/EKS_ROLE/${EKS_ROLE}" cluster-template.yaml > ${AWS_REGION}-${CLUSTER_NAME}.yaml
 # cluster creation
 ./eksctl create cluster -f ${AWS_REGION}-${CLUSTER_NAME}.yaml
 ./eksctl utils associate-iam-oidc-provider --region=${AWS_REGION} --cluster=${CLUSTER_NAME} --approve
 # make sure the role exist in AWS IAM
-./eksctl create iamidentitymapping --region ${AWS_REGION} --cluster ${CLUSTER_NAME}  --arn arn:aws:iam::${ARN_NUMBER}:role/EKS_ROLE --group system:masters --username admin
+./eksctl create iamidentitymapping --region ${AWS_REGION} --cluster ${CLUSTER_NAME}  --arn arn:aws:iam::${ARN_NUMBER}:role/${EKS_ROLE} --group system:masters --username admin
