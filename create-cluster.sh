5bf5a895383154f7464f2453ded70fc3dbd9d526
Shahar Femron <59650633+Shahar-Satori@users.noreply.github.com>
2021-01-23 22:42:34 +0200

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -8,14 +8,19 @@ PRIVATE_SUB1_ID="subnet-1a2bc34d"
 PRIVATE_SUB2_ID="subnet-1a2bc34d"
 PUBLIC_SUB1_ID="subnet-1a2bc34d"
 PUBLICS_SUB2_ID="subnet-1a2bc34d"
-CLUSTER_NAME='satori-dac'
+CLUSTER_NAME="satori-dac"
+EKS_ROLE="EKS_ROLE" 
+
+
 echo "Creating an AWS DAC with eksctl"
 # get eksctl.tar.gz
 curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" -o "eksctl.tar.gz"
 # open the tar.gz
 tar zxfv eksctl.tar.gz
+
 # create the cluster yaml
-sed "s/AWS_REGION/${AWS_REGION}/g;s/ARN_NUMBER/${ARN_NUMBER}/g;s/CLUSTER_NAME/${CLUSTER_NAME}/g;s/PRIVATE_SUB1_ID/${PRIVATE_SUB1_ID}/g;s/PRIVATE_SUB2_ID/${PRIVATE_SUB2_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB1_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB2_ID}/g;s/VPC_ID/${VPC_ID}/g;s/EKS_ROLE/${EKS_ROLE}" cluster-template.yaml > ${AWS_REGION}-${CLUSTER_NAME}.yaml
+sed "s/AWS_REGION/${AWS_REGION}/g;s/ARN_NUMBER/${ARN_NUMBER}/g;s/CLUSTER_NAME/${CLUSTER_NAME}/g;s/PRIVATE_SUB1_ID/${PRIVATE_SUB1_ID}/g;s/PRIVATE_SUB2_ID/${PRIVATE_SUB2_ID}/g;s/PUBLIC_SUB1_ID/${PUBLIC_SUB1_ID}/g;s/PUBLIC_SUB2_ID/${PUBLIC_SUB2_ID}/g;s/VPC_ID/${VPC_ID}/g;s/EKS_ROLE/${EKS_ROLE}/g" cluster-template.yaml > ${AWS_REGION}-${CLUSTER_NAME}.yaml
+
 # cluster creation
 ./eksctl create cluster -f ${AWS_REGION}-${CLUSTER_NAME}.yaml
 ./eksctl utils associate-iam-oidc-provider --region=${AWS_REGION} --cluster=${CLUSTER_NAME} --approve
